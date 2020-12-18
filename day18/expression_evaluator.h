d8efb5f60da18aa2ddc6d0943f8e677090cb9831
Adrian Scripca <benishor@gmail.com>
2020-12-18 10:45:15 +0200

    Day 18: complete

---
+++ day18/expression_evaluator.h
@@ -0,0 +1,93 @@
+//
+// Created by benny on 18/12/2020.
+//
+
+#ifndef AOC2K20_EXPRESSION_EVALUATOR_H
+#define AOC2K20_EXPRESSION_EVALUATOR_H
+
+#include <functional>
+#include <utility>
+
+// TODO: add proper error handling for validating expression syntax
+template<typename T>
+class expression_evaluator {
+public:
+
+    explicit expression_evaluator(std::function<int(char)> opPrecedence) : op_precedence(std::move(opPrecedence)) {}
+
+    T evaluate(const std::string& expression) {
+        std::stack<long> operands;
+        std::stack<char> operators;
+
+        for (size_t i = 0; i < expression.length(); i++) {
+            auto c = expression[i];
+            if (std::isspace(c)) {
+                continue;
+            } else if (c == '(') {
+                operators.push(c);
+            } else if (std::isdigit(c)) {
+                T value = 0;
+                while (i < expression.length() && std::isdigit(expression[i])) {
+                    value *= 10;
+                    value += expression[i] - '0';
+                    i++;
+                }
+                operands.push(value);
+                i--;
+            } else if (c == ')') {
+                while (!operators.empty() && operators.top() != '(') {
+                    auto op = operators.top();
+                    operators.pop();
+                    auto b = operands.top();
+                    operands.pop();
+                    auto a = operands.top();
+                    operands.pop();
+                    operands.push(apply_operation(op, a, b));
+                }
+                operators.pop(); // pop ( from the stack
+            } else {
+                // must be an operator
+                while (!operators.empty() && op_precedence(operators.top()) >= op_precedence(c)) {
+                    auto op = operators.top();
+                    operators.pop();
+                    auto b = operands.top();
+                    operands.pop();
+                    auto a = operands.top();
+                    operands.pop();
+                    operands.push(apply_operation(op, a, b));
+                }
+                operators.push(c);
+            }
+        }
+        while (!operators.empty()) {
+            auto op = operators.top();
+            operators.pop();
+            auto b = operands.top();
+            operands.pop();
+            auto a = operands.top();
+            operands.pop();
+            operands.push(apply_operation(op, a, b));
+        }
+        return operands.top();
+    }
+
+private:
+    std::function<int(char)> op_precedence;
+
+    T apply_operation(char operation, T a, T b) {
+        switch (operation) {
+            case '+':
+                return a + b;
+            case '*':
+                return a * b;
+            case '-':
+                return a - b;
+            case '/':
+                return a / b;
+            default:
+                return 0;
+        }
+    }
+};
+
+#endif //AOC2K20_EXPRESSION_EVALUATOR_H
--- day18/main.cpp
+++ day18/main.cpp
@@ -1,3 +1,64 @@
-int main() {
-    return 0;
+#include "solution.h"
+#include "expression_evaluator.h"
+
+int part1_operator_precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+int part2_operator_precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 2;
+        case '*':
+        case '/':
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+class day18 : public aoc::solution {
+protected:
+
+    void run(std::istream& in, std::ostream& out) override {
+
+        expression_evaluator<long> calculator_part1{part1_operator_precedence};
+        expression_evaluator<long> calculator_part2{part2_operator_precedence};
+
+        long part_1{0}, part_2{0};
+        for (std::string line; std::getline(in, line);) {
+            line = aoc::trim(line);
+            part_1 += calculator_part1.evaluate(line);
+            part_2 += calculator_part2.evaluate(line);
+        }
+        out << part_1 << std::endl;
+        out << part_2 << std::endl;
+    }
+
+    void register_tests() override {
+        add_test(aoc::test{
+                .name = "First",
+                .input = "1 + 2 * 3 + 4 * 5 + 6",
+                .expected_output = "71\n231"
+        });
+        add_test(aoc::test{
+                .name = "Second",
+                .input = "1+(2*3)+(4*(5+6))",
+                .expected_output = "51\n51"
+        });
+    }
+};
+
+int main(int argc, char** argv) {
+    day18 solution{};
+    return solution.execute(argc, argv);
 }
